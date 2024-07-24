f1366956bcbd8d45fcbc47b42e79efca0f66457b
min0622 <151921634+min0622@users.noreply.github.com>
2024-07-24 23:08:17 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -5,7 +5,7 @@ $(function () {
 
     mouseover: function () {
       $('header').addClass('on');
-      
+
       // 오버시, 밑줄발생
       $(this).find('.m').addClass('m_line')
       $('.header_inner nav .submenu, header::after').stop(true).slideDown();
@@ -52,6 +52,25 @@ $(function () {
 
     }
   });
+
+  // header _스크롤
+  $(function() {
+    var prevScrollTop = 0;
+  
+    document.addEventListener("scroll", function(){
+        
+        var nowScrollTop = $(window).scrollTop(); //현재 스크롤 위치를 nowScrollTop 에 저장
+        
+        if (nowScrollTop > prevScrollTop){ $('header').addClass('active'); } 
+        // 스크롤 방향(Down) 내릴때 -> 헤더에 active 클래스 추가
+        else { $('header').removeClass('active'); } // 스크롤 방향(Up) 올릴때 -> 헤더에 active 클래스 제거
+        prevScrollTop = nowScrollTop;  // prevScroll, nowScrollTop 조건 판단 후, 현재 스크롤값을 prevScrollTop 에 저장
+  
+    });
+  
+  });
+
+
 
 
 
@@ -154,6 +173,162 @@ $(function () {
 
 
   });
+
+
+
+
+  // gsap
+  let mm = gsap.matchMedia();
+
+
+mm.add("(min-width: 768px)", () => {
+    
+   
+    const ani = gsap.timeline();
+    ani.from('main .video_box',{xPercent:-50})
+                ani.to('main .video_box', { width: '100%', height: '100%', borderRadius: 120, duration: .5 })
+                    .to('main .video_box', { scale: 1.2 })
+                ScrollTrigger.create({
+                    animation: ani,
+                    trigger: "main",
+                    start: "2% top",
+                    end: "+=800",
+                    scrub: true,
+                    pin: true,
+                    markers: true,
+                    anticipatePin: 1
+                });
+    
+                
+});
+
+mm.add("(max-width: 767px)", () => {
+    
+    const ani_mobile = gsap.timeline();
+    ani_mobile.from('main .video_box',{yPercent:-50})
+    ani_mobile.to('main .video_box', { width: '100%', height: '100%', borderRadius: 100})
+                    .to('main .video_box', { scale: 1.2 })
+                ScrollTrigger.create({
+                    animation: ani_mobile,
+                    trigger: "main",
+                    start: "2% top",
+                    end: "+=100",
+                    scrub: true,
+                    pin: true,
+                    markers: true,
+                    anticipatePin: 1
+                });
+
+
+});
+
+        
+
+
+
+
+        // section_2
+        const section_2 = document.querySelector("#section_2");
+        const horizontal = document.querySelector("#horizontal");
+        const sections = gsap.utils.toArray("#horizontal > section");
+
+        gsap.to(sections, {
+            xPercent: -100 * (sections.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: section_2,
+                start: "top top",
+                // start: "-16% top",
+                end: () => "+=" + (horizontal.offsetWidth - innerWidth),
+                pin: true,
+                scrub: 1,
+                snap: {
+                    snapTo: 1 / (sections.length - 1),
+                    inertia: false,
+                    duration: { min: 0.1, max: 0.1 }
+                },
+                invalidateOnRefresh: true,
+                anticipatePin: 1,
+                markers: true
+            }
+        });
+
+
+
+        // header
+        // const showNav = gsap.from("header", {
+        //     yPercent: -200,
+        //     paused: true,
+        //     duration: 0.2
+        // }).progress(1);
+
+        // ScrollTrigger.create({
+        //     start: "top top",
+        //     end: 99999,
+        //     onUpdate: (self) => {
+        //         self.direction === -1 ? showNav.play() : showNav.reverse()
+        //     }
+        // });
+
+
+
+        
+        var swiper = new Swiper(".mySwiper", {
+          slidesPerView: 1.2,
+          spaceBetween: 20,
+          centeredSlides: true,
+          loop:true,
+          // pagination: {
+          //   el: ".swiper-pagination",
+          //   clickable: true,
+          // },
+          breakpoints: {
+   
+            431:{
+                slidesPerView: 1.2, 
+                spaceBetween: 30,
+            },
+            576:{
+                slidesPerView: 1.5, 
+                spaceBetween: 50
+    
+            },
+            768:{
+                slidesPerView: 2, 
+                spaceBetween: 40,
+    
+            },
+            992:{
+                slidesPerView: 2, 
+                spaceBetween: 70,
+    
+            },
+            1020:{
+                slidesPerView: 2.5, 
+                spaceBetween: 50,
+    
+            },
+          1400: {
+              slidesPerView: 2.5, 
+              spaceBetween: 140,
+              loop:false
+
+            },
+            1600 : {
+              slidesPerView: 3, 
+              spaceBetween: 90,
+              loop:false
+            }
+            // 1600 : {
+            //   slidesPerView: 3, 
+            //   spaceBetween: 90,
+            //   loop:false
+            // },
+          },
+        });
+
+
+  
 
 
 
